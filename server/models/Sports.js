2ea35b3b49e20e0454f2bdb51a4970d7f75c49b8
Xinyan Lv <lvxinyan1992@gmail.com>
2016-04-11 12:40:31 +0100

    Upload the MongoDB commands

--- server/models/Sports.js
+++ server/models/Sports.js
@@ -2,23 +2,23 @@ var mongoose = require('mongoose');
 
 // Create the FoodSchema.
 var SportsSchema = new mongoose.Schema({
-  'Activity, Exercise or Sport (1 hour)': {
+  "Activity, Exercise or Sport (1 hour)": {
     type: String,
     required: true
   },
-  '130 lb': {
+  "130 lb": {
     type: String,
     required: true
   },
-  '155 lb': {
+  "155 lb": {
     type: String,
     required: true
   },
-  '180 lb': {
+  "180 lb": {
     type: String,
     required: true
   },
-  '205 lb': {
+  "205 lb": {
     type: String,
     required: true
   }
