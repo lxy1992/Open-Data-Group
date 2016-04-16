7181c71125276ff62a1efe72438c9342a28ef731
CherishIt <Zhou@Zhous-MacBook.local>
2016-04-16 14:08:34 +0100

    merge conflict

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/lodash/lodash.js',
       'bower_components/restangular/dist/restangular.js',
+      'bower_components/ng-table/dist/ng-table.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
