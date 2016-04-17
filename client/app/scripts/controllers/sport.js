b5619ecbbeddddbc150ec828d6273eeaf1378554
Xinyan Lv <lvxinyan1992@gmail.com>
2016-04-17 13:34:20 +0100

    Add Page Shifting

--- client/app/scripts/controllers/sport.js
+++ client/app/scripts/controllers/sport.js
@@ -8,10 +8,36 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('SportCtrl', function ($scope, Sport) {
-    $scope.sport = Sport.getList().$object;
+  .controller('SportCtrl', function ($scope, Sport, NgTableParams) {
+    
     $scope.setWeight = function(weight){
-      return 
+      return
     };
+    $scope.sports = [];
+    Sport.getList().then(function(data){
+        $scope.sports = data;
+        $scope.tableParams = new NgTableParams({
+      count: 5 //每页显示数量
+    }, {
+        paginationMaxBlocks: 5,
+        paginationMinBlocks: 2,
+        data: $scope.sports
+    });
+        
+        console.log($scope.sports);
+//        $scope.tableParams.reload();
+        $scope.applyGlobalSearch = applyGlobalSearch;
+        function applyGlobalSearch(){
+            console.log("aa");
+            var term = $scope.globalSearchTerm;
+            console.log(term);
+            if ($scope.isInvertedSearch){
+                term = "!" + term;
+      }
+            $scope.tableParams.filter({ $: term });
+        }
+    });
+    
+    
 
   });
