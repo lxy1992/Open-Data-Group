8b5ede2b90288ba95465ff0ea9eb3013a6303e63
358203708 <358203708@qq.com>
2016-04-19 00:28:33 +0100

    update about page

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function (config) {
       'bower_components/highcharts/highcharts.js',
       'bower_components/highcharts/highcharts-more.js',
       'bower_components/highcharts/modules/exporting.js',
+      'bower_components/bootstrap-progressbar/bootstrap-progressbar.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js'
