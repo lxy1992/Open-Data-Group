3524060d1e6a6fe8d26ff74d6aab68425239308f
CherishIt <Zhou@Zhous-MacBook.local>
2016-04-26 23:14:37 +0100

    Revert "Add the Datapicker, Timepicker, Datepair"
    This reverts commit 014d85797cebc80e6475d1f9f72e0c63882880c7.

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -38,7 +38,6 @@ module.exports = function (config) {
       'bower_components/bootstrap-progressbar/bootstrap-progressbar.js',
       'bower_components/moment/moment.js',
       'bower_components/fullcalendar/dist/fullcalendar.js',
-      'bower_components/jt.timepicker/jquery.timepicker.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js'
